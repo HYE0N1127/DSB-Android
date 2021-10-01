812ca13b912d014e41d2411d61394b959058341d
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-10-01 20:21:03 +0900

    Modify : MVVM 구조 적용

---
+++ presentation/src/main/java/kr/hs/dgsw/dsb_android/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.dsb_android.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.hs.dgsw.dsb_android.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- presentation/src/main/java/kr/hs/dgsw/dsb_android/ui/main/MainActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/dsb_android/ui/main/MainActivity.kt
@@ -1,9 +1,11 @@
-package kr.hs.dgsw.dsb_android
+package kr.hs.dgsw.dsb_android.ui.main
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.hs.dgsw.dsb_android.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
