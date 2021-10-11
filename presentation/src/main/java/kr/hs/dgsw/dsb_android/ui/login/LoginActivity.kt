bff1c7ae5c362ded7b3906cdcce248f228b15c90
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-10-11 20:09:05 +0900

    ADD : Login -> Register Activity 이벤트 추가

--- presentation/src/main/java/kr/hs/dgsw/dsb_android/ui/login/LoginActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/dsb_android/ui/login/LoginActivity.kt
@@ -1,10 +1,23 @@
 package kr.hs.dgsw.dsb_android.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import kr.hs.dgsw.dsb_android.R
+import android.content.Intent
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import kr.hs.dgsw.dsb_android.RegisterActivity
 import kr.hs.dgsw.dsb_android.base.BaseActivity
+import kr.hs.dgsw.dsb_android.databinding.ActivityLoginBinding
 
-class LoginActivity : BaseActivity() {
+@AndroidEntryPoint
+class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(){
+    override val viewModel: LoginViewModel by viewModels()
+
+    override fun observerViewModel() {
+        with(mViewModel) {
+            onRegisterClickEvent.observe(this@LoginActivity, {
+                val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            })
+        }
+    }
 
 }
