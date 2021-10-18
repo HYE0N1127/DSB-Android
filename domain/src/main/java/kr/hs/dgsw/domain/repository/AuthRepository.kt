dd0be884b39b13800600fbd148f8aab5d31a6f4d
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-10-18 20:43:56 +0900

    ADD : AuthRepository

---
+++ domain/src/main/java/kr/hs/dgsw/domain/repository/AuthRepository.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.domain.repository
+
+import io.reactivex.Completable
+
+interface AuthRepository {
+
+    fun register(id: String, pw: String, name: String) : Completable
+
+    fun login(id: String, pw: String) : Completable
+
+    fun simpleLogin(simplePw : String) : Completable
+}
