3281c7dc7864b6052d46b92e5fb77a9ff8aa21ec
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-10-28 19:33:18 +0900

    ADD : TokenRepository, TokenException

---
+++ data/src/main/java/kr/hs/dgsw/data/repository/TokenRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package kr.hs.dgsw.data.repository
+
+import io.reactivex.Completable
+import io.reactivex.Single
+import kr.hs.dgsw.data.datasource.TokenDataSource
+import kr.hs.dgsw.domain.entity.Token
+import kr.hs.dgsw.domain.repository.TokenRepository
+import javax.inject.Inject
+
+class TokenRepositoryImpl @Inject constructor(
+    private val tokenDataSource: TokenDataSource
+) : TokenRepository {
+    override fun getToken(): Single<Token> {
+        return tokenDataSource.getToken()
+    }
+
+    override fun deleteToken(): Completable {
+        TODO("Not yet implemented")
+    }
+
+}
