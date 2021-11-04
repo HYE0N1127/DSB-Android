442f40fe13be6689c8ec9d4bb3ca3cf10d387431
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-11-04 10:31:30 +0900

    Update : Gradle (Hilt -> Koin)

--- data/src/main/java/kr/hs/dgsw/data/base/BaseDataSource.kt
+++
@@ -1,6 +0,0 @@
-package kr.hs.dgsw.data.base
-
-abstract class BaseDataSource<RT, CH> {
-    abstract val remote: RT
-    abstract val cache: CH
-}
--- presentation/src/main/java/kr/hs/dgsw/dsb_android/DSBApplication.kt
+++ presentation/src/main/java/kr/hs/dgsw/dsb_android/DSBApplication.kt
@@ -1,15 +1,17 @@
 package kr.hs.dgsw.dsb_android
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-import kr.hs.dgsw.data.database.Preferences
+import org.koin.core.context.startKoin
 
-@HiltAndroidApp
 class DSBApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Preferences.init(this)
+        startKoin {
+            val module = listOf(
+
+            )
+        }
     }
 
 }
