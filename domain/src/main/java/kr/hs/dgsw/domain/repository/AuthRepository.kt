151e82f2f997b7e21768a93394cd8e83d0e5dbf8
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-11-09 20:22:55 +0900

    ADD : About authRepository function

--- domain/src/main/java/kr/hs/dgsw/domain/repository/AuthRepository.kt
+++ domain/src/main/java/kr/hs/dgsw/domain/repository/AuthRepository.kt
@@ -4,14 +4,22 @@ import io.reactivex.Completable
 
 interface AuthRepository {
 
-    fun register(id: String, pw: String, name: String) : Completable
+    fun register(
+        id: String,
+        pw: String,
+        name: String,
+        nickname: String,
+        phone: String,
+        birth: String,
+        profileImage: String,
+    ): Completable
 
-    fun login(id: String, pw: String) : Completable
+    fun login(id: String, pw: String): Completable
 
-    fun simpleRegister(userId: String, easyPassword: String) : Completable
+    fun simpleRegister(userId: String, easyPassword: String): Completable
 
-    fun simpleLogin(simplePw : String) : Completable
+    fun simpleLogin(idx: String, simplePassword: String): Completable
 
-    fun idDoubleValidCheck(id: String) : Completable
+    fun idDoubleValidCheck(id: String): Completable
 
 }
