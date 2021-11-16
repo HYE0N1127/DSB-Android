34411ec48e64b18d881a156207b33adb4c82e658
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-11-16 15:54:06 +0900

    ADD : ID 중복확인 기능 추가

--- presentation/src/main/java/kr/hs/dgsw/dsb_android/viewmodel/activity/RegisterViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/dsb_android/viewmodel/activity/RegisterViewModel.kt
@@ -1,18 +1,29 @@
 package kr.hs.dgsw.dsb_android.viewmodel.activity
 
+import android.view.View
 import androidx.lifecycle.MutableLiveData
+import io.reactivex.observers.DisposableCompletableObserver
+import kr.hs.dgsw.domain.request.auth.IdDoubleValidRequest
+import kr.hs.dgsw.domain.usecase.auth.IdDoubleValidUseCase
 import kr.hs.dgsw.dsb_android.base.BaseViewModel
 import kr.hs.dgsw.dsb_android.util.SingleLiveEvent
 
-class RegisterViewModel : BaseViewModel() {
+class RegisterViewModel(
+    private val idDoubleValidUseCase: IdDoubleValidUseCase,
+) : BaseViewModel() {
+
     val id = MutableLiveData<String>()
     val pw = MutableLiveData<String>()
     val name = MutableLiveData<String>()
+    val personalNumber = MutableLiveData<String>()
     val nickName = MutableLiveData<String>()
     val email = MutableLiveData<String>()
+    val phoneNumber = MutableLiveData<String>()
 
+    val idDoubleValidEvent = MutableLiveData(false)
     val emptyEvent = MutableLiveData(true)
     val registerEvent = SingleLiveEvent<Unit>()
+    val picProfileImageEvent = SingleLiveEvent<Unit>()
 
     val isAgree = MutableLiveData(false)
 
@@ -22,5 +33,23 @@ class RegisterViewModel : BaseViewModel() {
 
     fun onClickRegister() {
         registerEvent.call()
+    }
+
+    fun onClickProfileImage() {
+        picProfileImageEvent.call()
+    }
+
+    fun onClickIdAvailable(view: View) {
+        addDisposable(idDoubleValidUseCase.buildUseCaseObservable(IdDoubleValidUseCase.Params(id.value!!)),
+            object : DisposableCompletableObserver() {
+                override fun onComplete() {
+                    idDoubleValidEvent.value = true
+                }
+
+                override fun onError(e: Throwable) {
+                    idDoubleValidEvent.value = false
+                }
+
+            })
     }
 }
