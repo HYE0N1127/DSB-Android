a4b6f7cee6301a6c16a1aa8323b9a292f5327d2e
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-11-29 14:03:11 +0900

    Modify : Account Data Class

--- data/src/main/java/kr/hs/dgsw/data/repository/UploadRepositoryImpl.kt
+++ data/src/main/java/kr/hs/dgsw/data/repository/UploadRepositoryImpl.kt
@@ -3,14 +3,17 @@ package kr.hs.dgsw.data.repository
 import io.reactivex.Single
 import kr.hs.dgsw.data.datasource.UploadDataSource
 import kr.hs.dgsw.domain.repository.UploadRepository
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import org.json.JSONObject
 import java.io.File
 
 class UploadRepositoryImpl(private val uploadDataSource: UploadDataSource) : UploadRepository {
 
     override fun uploadImage(image: File?): Single<String> {
-
+        TODO()
     }
 }
